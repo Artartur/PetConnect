c8185c8ebb04c1851997e0c52e6d3af6e9b8aedd
artartur <arturgfs@hotmail.com>
2023-05-17 22:41:08 -0300

    Add style for Main and Login Component

---
+++ styles/styles.tsx
@@ -0,0 +1,70 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+});
+
+export const stylesLogin = StyleSheet.create({
+  buttonContainer: {
+    alignItems: "center",
+    backgroundColor: "#AB8262",
+    borderRadius: 10,
+    height: 60,
+    justifyContent: "center",
+    marginBottom: "5%",
+    width: "80%",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+  },
+  checkbox: {
+    borderColor: "gray",
+    borderRadius: 3,
+    borderWidth: 1,
+    height: 24,
+    marginBottom: "10%",
+    marginRight: 6,
+    width: 24,
+  },
+  checkBoxContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 5,
+    width: "80%",
+  },
+  container: {
+    marginBottom: "10%",
+    top: 50,
+  },
+  forgotPassword: {
+    marginLeft: "17%",
+  },
+  markup: {
+    color: "#E88181",
+  },
+  text: {
+    fontSize: 18,
+  },
+  textInputEmail: {
+    backgroundColor: "#C4C4C433",
+    borderRadius: 10,
+    fontSize: 16,
+    height: 60,
+    marginBottom: "5%",
+    paddingHorizontal: 12,
+    width: "80%",
+  },
+  textInputPassword: {
+    backgroundColor: "#C4C4C433",
+    borderRadius: 10,
+    fontSize: 16,
+    height: 60,
+    marginBottom: "3%",
+    paddingHorizontal: 12,
+    width: "80%",
+  },
+});
