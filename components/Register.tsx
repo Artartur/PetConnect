4f4d947aee97f0100a840bff5563f024564eaa99
artartur <arturgfs@hotmail.com>
2023-05-23 01:18:09 -0300

    Create Register component

---
+++ components/Register.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Image, View } from "react-native";
+import Checkbox from "./Checkbox";
+import Button from "./Button";
+import {
+  ColorsOptions,
+  InputModeOptions,
+  KeyboardTypeOptions,
+} from "../types/enums";
+import InputWithIcon from "./InputWithIcon";
+import { stylesRegister } from "../styles/styles";
+
+export default function Register() {
+  return (
+    <>
+      <View style={stylesRegister.container}>
+        <Image
+          style={stylesRegister.image}
+          source={require("../assets/logo.png")}
+        />
+        <InputWithIcon
+          color={ColorsOptions.gray}
+          inputMode={InputModeOptions.text}
+          keyboardType={KeyboardTypeOptions.default}
+          name={"md-person"}
+          placeholder={"Nome"}
+          size={24}
+        />
+        <InputWithIcon
+          color={ColorsOptions.gray}
+          inputMode={InputModeOptions.email}
+          keyboardType={KeyboardTypeOptions.emailaddress}
+          name={"mail"}
+          placeholder={"Email"}
+          size={24}
+        />
+        <InputWithIcon
+          color={ColorsOptions.gray}
+          inputMode={InputModeOptions.tel}
+          keyboardType={KeyboardTypeOptions.numberpad}
+          name={"phone-portrait-outline"}
+          placeholder={"Numero de telefone"}
+          size={24}
+        />
+        <InputWithIcon
+          color={ColorsOptions.gray}
+          inputMode={InputModeOptions.text}
+          keyboardType={KeyboardTypeOptions.default}
+          name={"lock-closed"}
+          placeholder={"Senha"}
+          size={24}
+        />
+
+        <Checkbox text={"Li e aceito os termos de uso"} />
+        <Button text={"Cadastrar"} />
+      </View>
+    </>
+  );
+}
