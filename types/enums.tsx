2225c8cf94bdb53b80156951a1b04f2f7be57601
artartur <arturgfs@hotmail.com>
2023-05-23 01:16:43 -0300

    Formating

--- types/enums.tsx
+++ types/enums.tsx
@@ -1,33 +1,31 @@
-
-export enum ColorsOptions{
-    black = 'black',
-    blue = 'blue',
-    gray = 'gray',
-    green = 'green',
-    purple = 'purple',
-    red = 'red',
-    white = 'white',
-    yellow = 'yellow',
+export enum ColorsOptions {
+  black = "black",
+  blue = "blue",
+  gray = "gray",
+  green = "green",
+  purple = "purple",
+  red = "red",
+  white = "white",
+  yellow = "yellow",
 }
 
-export enum InputModeOptions{
-    decimal = 'decimal',
-    email = 'email',
-    none = 'none',
-    numeric = 'numeric',
-    search = 'search',
-    text = 'text',
-    tel = 'tel',
-    url = 'url'
+export enum InputModeOptions {
+  decimal = "decimal",
+  email = "email",
+  none = "none",
+  numeric = "numeric",
+  search = "search",
+  text = "text",
+  tel = "tel",
+  url = "url",
 }
 
-export enum KeyboardTypeOptions{
-    decimalpad = 'decimal-pad',
-    default = 'default',
-    emailaddress = 'email-address',
-    numberpad = 'number-pad',
-    numeric = 'numeric',
-    phonepad = 'phone-pad',
-    url = 'url'
+export enum KeyboardTypeOptions {
+  decimalpad = "decimal-pad",
+  default = "default",
+  emailaddress = "email-address",
+  numberpad = "number-pad",
+  numeric = "numeric",
+  phonepad = "phone-pad",
+  url = "url",
 }
-
