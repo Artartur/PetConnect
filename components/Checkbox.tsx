2bd8ce8852f565a7a53e354de795db19983b7dff
artartur <arturgfs@hotmail.com>
2023-05-23 01:17:20 -0300

    Create checkbox component

---
+++ components/Checkbox.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
+import { stylesCheckbox } from "../styles/styles";
+import { Checkboxs } from "../types/types";
+
+export default function Checkbox({ text }: Checkboxs) {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheckboxPress = () => {
+    setChecked(!checked);
+  };
+
+  const checkBoxStyle = [
+    stylesCheckbox.checkbox,
+    { backgroundColor: checked ? "#32CD32" : "transparent" },
+  ];
+
+  return (
+    <>
+      <TouchableOpacity onPress={handleCheckboxPress}>
+        <View style={stylesCheckbox.checkBoxContainer}>
+          <View style={checkBoxStyle}></View>
+          <Text>{text}</Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  );
+}
