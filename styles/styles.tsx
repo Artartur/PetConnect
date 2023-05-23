ed63b813766632dc7919c54e27b2ba3a97f61af8
artartur <arturgfs@hotmail.com>
2023-05-23 01:23:59 -0300

    Create some stylesSheet for each component created

--- styles/styles.tsx
+++ styles/styles.tsx
@@ -9,14 +9,14 @@ export const styles = StyleSheet.create({
   },
 });
 
-export const stylesLogin = StyleSheet.create({
+export const stylesButton = StyleSheet.create({
   buttonContainer: {
     alignItems: "center",
     backgroundColor: "#AB8262",
     borderRadius: 10,
     height: 60,
     justifyContent: "center",
-    marginBottom: "5%",
+    marginTop: "5%",
     width: "80%",
   },
 
@@ -24,38 +24,34 @@ export const stylesLogin = StyleSheet.create({
     color: "white",
     fontSize: 18,
   },
+})
 
+export const stylesCheckbox = StyleSheet.create({
   checkbox: {
     borderColor: "gray",
-    borderRadius: 3,
-    borderWidth: 1,
+    borderRadius: 4,
+    borderWidth: 2,
     height: 24,
-    marginBottom: "10%",
     marginHorizontal: 5,
     width: 24,
   },
 
   checkBoxContainer: {
-    justifyContent: "center",
-    textAlign: "center",
+    alignItems: "center",
     flexDirection: "row",
-    width: "50%",
-    marginHorizontal: "2%",
   },
+});
 
-  forgotPasswordContainer: {
-    width: "50%",
-  },
+export const stylesLogin = StyleSheet.create({
 
-  imageContainer: {
-    marginRight: "5%",
-  },
+  imageContainer: {},
 
   managerAccessContainer: {
+    alignItems: "center",
     flexDirection: "row",
-    justifyContent: "center",
+    justifyContent: "space-around",
+    marginBottom: "5%",
     width: "100%",
-    marginLeft: "5%",
   },
 
   markup: {
@@ -65,7 +61,7 @@ export const stylesLogin = StyleSheet.create({
   text: {
     fontSize: 18,
   },
-});
+})
 
 export const stylesInput = StyleSheet.create({
   container: {
@@ -89,4 +85,18 @@ export const stylesInput = StyleSheet.create({
     paddingHorizontal: 12,
     width: "80%",
   },
+});
+
+export const stylesRegister = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "100%"
+  },
+  image:{
+    width: 250,
+    height: 250,
+  }
 });
