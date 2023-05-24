c5e6959e3158d08b8af4f77b847045994ce3e5ad
artartur <arturgfs@hotmail.com>
2023-05-24 00:45:02 -0300

    add onPress type in Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,10 +4,10 @@ import { Text, TouchableOpacity } from "react-native";
 import { stylesButton } from "../styles/styles";
 import { Buttons } from "../types/types";
 
-export default function ({ text }: Buttons){
+export default function ({ text, onPress}: Buttons){
   return (
     <>
-      <TouchableOpacity style={stylesButton.buttonContainer}>
+      <TouchableOpacity style={stylesButton.buttonContainer} onPress={onPress}>
         <Text style={stylesButton.buttonText}>{text}</Text>
       </TouchableOpacity>
     </>
--- types/types.tsx
+++ types/types.tsx
@@ -1,15 +1,16 @@
 import { InputModeOptions, KeyboardTypeOptions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-export interface Cards{
-  text:string;
+export interface Cards {
+  text: string;
   children?: JSX.Element | JSX.Element[];
 }
 
-export interface Checkboxs extends Cards {
-}
+export interface Checkboxs extends Cards {}
 
-export interface Buttons extends Cards {}
+export interface Buttons extends Cards {
+  onPress(params: any): void;
+}
 
 export interface defaultProps {
   value?: string;
