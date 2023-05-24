de3567c3e7c800a7778fee60348fe23265c12bd2
artartur <arturgfs@hotmail.com>
2023-05-23 21:41:01 -0300

    add Card component in MainScreen

--- components/MainScreen.tsx
+++ components/MainScreen.tsx
@@ -1,51 +1,52 @@
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
+
+import Card from "./Card";
+import { ColorsOptions } from "../types/enums";
 import { stylesMainScreen } from "../styles/styles";
+
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function MainScreen() {
-
-    const iconRotation = [{transform: [{rotate: '-20deg'}]}, stylesMainScreen.icon]
+  const iconRotation = [
+    { transform: [{ rotate: "-20deg" }] },
+    stylesMainScreen.icon,
+  ];
 
   return (
     <>
       <TouchableOpacity style={stylesMainScreen.navigator}>
         <Ionicons name={"arrow-back"} size={30} color={"#AB8262"} />
       </TouchableOpacity>
+
       <View style={stylesMainScreen.container}>
-        <TouchableOpacity style={stylesMainScreen.card}>
-          <View style={stylesMainScreen.view}>
-            <Ionicons
-              style={iconRotation}
-              name={"volume-high"}
-              size={40}
-              color={"white"}
-            />
-            <Text>Reportar Animal Abandonado</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={stylesMainScreen.card}>
-          <View style={stylesMainScreen.view}>
-            <Ionicons
-              style={stylesMainScreen.icon}
-              name={"search"}
-              size={40}
-              color={"white"}
-            />
-            <Text>Adotar um Pet</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={stylesMainScreen.card}>
-          <View style={stylesMainScreen.view}>
-            <Ionicons
-              style={stylesMainScreen.icon}
-              name={"analytics"}
-              size={40}
-              color={"white"}
-            />
-            <Text>Ver status de animal reportado</Text>
-          </View>
-        </TouchableOpacity>
+        <Card text={"Reportar um animal abandonado"}>
+          <Ionicons
+            style={iconRotation}
+            name={"volume-high"}
+            size={40}
+            color={ColorsOptions.white}
+          />
+        </Card>
+
+        <Card text={"Adotar um Pet"}>
+          <Ionicons
+            style={stylesMainScreen.icon}
+            name={"search"}
+            size={40}
+            color={ColorsOptions.white}
+          />
+        </Card>
+
+        <Card text={"Status de animais reportados"}>
+          <Ionicons
+            style={stylesMainScreen.icon}
+            name={"analytics"}
+            size={40}
+            color={ColorsOptions.white}
+          />
+        </Card>
+
       </View>
     </>
   );
