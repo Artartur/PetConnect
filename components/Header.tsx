8fc1152e46db33cee794de215602e7ca0556149f
artartur <arturgfs@hotmail.com>
2023-05-26 01:34:30 -0300

    add showIcon props in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar, Text, TouchableOpacity, View } from "react-native";
 
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { stylesHeader } from "../styles/styles";
-import { Cards } from "../types/types";
+import { Headers } from "../types/types";
 
-export default function Header({ text }: Cards) {
+export default function Header({ showIcon, text }: Headers) {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <View style={stylesHeader.header}>
-        <TouchableOpacity style={stylesHeader.navigator}>
-          <Ionicons name={"arrow-back"} size={30} color={"#AB8262"} />
-        </TouchableOpacity>
+        {showIcon && (
+          <TouchableOpacity style={stylesHeader.navigator}>
+            <Ionicons name={"arrow-back"} size={30} color={"#AB8262"} />
+          </TouchableOpacity>
+        )}
         <Text style={stylesHeader.text}>{text}</Text>
       </View>
     </>
