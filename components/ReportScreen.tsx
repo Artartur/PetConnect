379df2ca6e90f3469e195064f4e8833a4bcf936d
artartur <arturgfs@hotmail.com>
2023-05-28 15:17:13 -0300

    add real-time geolocation

--- components/ReportScreen.tsx
+++ components/ReportScreen.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import axios from "axios";
+import * as Location from "expo-location";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
 
 import InputWithIcon from "./InputWithIcon";
 import Button from "./Button";
@@ -16,13 +18,62 @@ import { stylesReport } from "../styles/styles";
 import Select from "./Select";
 
 export default function ReportScreen() {
+  const [address, setAddress] = useState(false);
+  const [bairro, setBairro] = useState("");
+  const [cidade, setCidade] = useState("");
   const [isFirstButtonVisible, setFirstButtonVisible] = useState(true);
   const [isSecondButtonVisible, setSecondButtonVisible] = useState(true);
-  const [isAddAddress, setAddress] = useState(false);
+  const [lat, setLat] = useState(0);
+  const [lon, setLon] = useState(0);
+  const [rua, setRua] = useState("");
   const [selectedValue, setSelectedValue] = useState("");
+
+  const fetchLocationAndAndress = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      try {
+        const location = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = location.coords;
+        setLat(latitude);
+        setLon(longitude);
+        getAddressFromCoordinates(latitude, longitude);
+      } catch (err: any) {
+        Alert.alert(
+          "Ops! aconteceu um erro:",
+          "A solicitacao de localizacao foi negada, favor permitir ou apertar o botao para inserir manualmente o endereco"
+        );
+      }
+    }
+  };
+
+  const getAddressFromCoordinates = async (
+    latitude: number,
+    longitude: number
+  ) => {
+    try {
+      const response = await axios.get(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+      );
+      const data = response.data;
+      if (data && data.address && data.address.postcode) {
+        const { city, road, suburb } = data.address;
+        setBairro(suburb || "");
+        setCidade(city || "");
+        setRua(road || "");
+      } else {
+        setBairro("Bairro nao encontrado");
+        setCidade("Cidade nao encontrada");
+        setRua("Rua nao encontrada");
+      }
+    } catch (err) {
+      Alert.alert("Ops! aconteceu um erro: ", "Endereco nao encontrado");
+    }
+  };
+
   const handleFirstButtonVisible = () => {
     setFirstButtonVisible(false);
     setSecondButtonVisible(false);
+    fetchLocationAndAndress();
   };
 
   const handleSecondButtonVisible = () => {
@@ -55,7 +106,7 @@ export default function ReportScreen() {
               keyboardType={KeyboardTypeOptions.default}
               name={"md-person"}
               placeholder="Nome"
-              size={24}
+              size={20}
             />
 
             <InputWithIcon
@@ -65,7 +116,7 @@ export default function ReportScreen() {
               keyboardType={KeyboardTypeOptions.emailaddress}
               name={"mail"}
               placeholder="Email"
-              size={24}
+              size={20}
             />
 
             <InputWithIcon
@@ -75,7 +126,7 @@ export default function ReportScreen() {
               keyboardType={KeyboardTypeOptions.numeric}
               name={"phone-portrait"}
               placeholder="Numero do telefone"
-              size={24}
+              size={20}
             />
 
             <Select
@@ -85,30 +136,30 @@ export default function ReportScreen() {
               value={selectedValue}
             />
 
-            {isAddAddress && (
+            {address && (
               <>
                 <InputWithIcon
                   color={ColorsOptions.gray}
                   containerStyle={style.input}
-                  name={"add"}
+                  name={"location"}
                   placeholder={"Rua"}
-                  size={24}
+                  size={20}
                 />
 
                 <InputWithIcon
                   color={ColorsOptions.gray}
                   containerStyle={style.input}
-                  name={"add"}
+                  name={"location"}
                   placeholder={"Bairro"}
-                  size={24}
+                  size={20}
                 />
 
                 <InputWithIcon
                   color={ColorsOptions.gray}
                   containerStyle={style.input}
-                  name={"add"}
+                  name={"location"}
                   placeholder={"Cidade"}
-                  size={24}
+                  size={20}
                 />
               </>
             )}
@@ -120,7 +171,7 @@ export default function ReportScreen() {
               keyboardType={KeyboardTypeOptions.default}
               name={"pencil"}
               placeholder="Descricao"
-              size={24}
+              size={20}
             />
 
             <View style={stylesReport.buttons}>
